223ef2fa2aa456a951bf6fca0f3365fb423e3344
Satish Tiyyagura - s0t02zw <Satish.Tiyyagura@walmart.com>
2020-04-05 17:51:25 -0500

    Spring Web and lombok were added to pom and src, test folders are created.

---
+++ src/test/java/com/stream/stream/tiyyagura/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.stream.tiyyagura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
